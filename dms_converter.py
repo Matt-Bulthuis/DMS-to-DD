876c54674e077b71dcef03d03e30defcaccb3dcb
mfbult1217@ung.edu <laxrules227>
2018-06-11 17:26:36 -0400

    initial commit gitignore, dms converter, requirements

--- dms_converter.py
+++ dms_converter.py
@@ -1,7 +1,18 @@
-"""Takes degrees minutes seconds coordinates and returns them in decimal degrees
+"""Takes decimal degrees coordinates and returns them in degrees minutes seconds
 
 """
 import pyperclip
-def dms_to_dd(d, m, s):
-    dd = d + float(m)/60 + float(s)/3600
-    return dd
+
+def decdeg2dms(dd):
+    negative = dd < 0
+    dd = abs(dd)
+    minutes,seconds = divmod(dd*3600,60)
+    degrees,minutes = divmod(minutes,60)
+    if negative:
+        if degrees > 0:
+            degrees = -degrees
+        elif minutes > 0:
+            minutes = -minutes
+        else:
+            seconds = -seconds
+    return (degrees,minutes,seconds)
